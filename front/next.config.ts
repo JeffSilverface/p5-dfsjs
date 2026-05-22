7cf9906f3d5c45fa3cf48fe322b45d7baaa5f85e
Jean-François PANN <78153059+JeffSilverface@users.noreply.github.com>
2026-05-22 09:30:49 +0200

    import bug corrected

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   turbopack: {
     resolveAlias: {
-      "@shared": path.resolve(__dirname, "../shared/src"),
+      "@shared": "../shared/src",
     },
   },
 };
