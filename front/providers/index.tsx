8c241d4f3c47645009c44c5c69d309e896e04117
Jean-François PANN <78153059+JeffSilverface@users.noreply.github.com>
2026-05-22 11:15:57 +0200

    get/me is op

--- front/providers/index.tsx
+++ front/providers/index.tsx
@@ -1,12 +1,19 @@
-'use client'
+"use client";
 
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from '@/lib/query-client'
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/query-client";
+import { useSession } from "@/hooks/useSession";
+
+function SessionLoader() {
+  useSession();
+  return null;
+}
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
+      <SessionLoader />
       {children}
     </QueryClientProvider>
-  )
+  );
 }
