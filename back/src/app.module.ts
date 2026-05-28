0fb8b39320892d62b2cf25b751012c9c2fd2da9b
Jean-François PANN <78153059+JeffSilverface@users.noreply.github.com>
2026-05-28 14:19:59 +0200

    Remove shared folder

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,12 +2,17 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
 import { ArticlesModule } from './features/articles/articles.module';
 import { AuthenticatedGuard } from './common/guards/authenticated.guard';
+import { AuthModule } from './features/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule, ArticlesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    ArticlesModule,
+  ],
   providers: [{ provide: APP_GUARD, useClass: AuthenticatedGuard }],
 })
 export class AppModule {}
--- shared/src/schemas/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './user.schema'
-export * from './article.schema'
-export * from './comment.schema'
