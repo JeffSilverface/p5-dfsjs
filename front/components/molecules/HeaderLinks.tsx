714e0bc3ba1ad4697891af1e6c51e244397ce0b2
Jean-François PANN <78153059+JeffSilverface@users.noreply.github.com>
2026-06-02 12:23:14 +0200

    optimisation n1

--- front/components/molecules/HeaderLinks.tsx
+++ front/components/molecules/HeaderLinks.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import Link from "next/link";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Avatar, AvatarFallback } from "../ui/avatar";
 import { Separator } from "../ui/separator";
 import { useUIStore } from "@/store/ui.store";
 
 export function HeaderLinks() {
   const closeMenu = useUIStore((state) => state.closeMenu);
+  const user = useUIStore((state) => state.user);
+  const initials = user?.username?.slice(0, 2).toUpperCase() ?? "??";
+
 
   return (
     <nav className="flex flex-col md:flex-row gap-4 items-end md:items-center p-4 md:p-0 h-full">
@@ -22,11 +25,7 @@ export function HeaderLinks() {
       <Separator className="md:hidden mt-auto" />
       <Link href="/profile" onClick={closeMenu}>
         <Avatar size="lg" className="cursor-pointer hover:ring-2 hover:ring-purple-500">
-          <AvatarImage
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Atkinson_Rowan_crop.jpg/250px-Atkinson_Rowan_crop.jpg"
-            alt="Mr Bean"
-          />
-          <AvatarFallback>RA</AvatarFallback>
+          <AvatarFallback>{initials}</AvatarFallback>
         </Avatar>
       </Link>
     </nav>
